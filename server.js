a3c53412a27aa7dd657699253418d7eb9d540b0d
alex <alexander.vrabel@proton.me>
2026-04-04 20:23:22 +0200

    added the frontend folder

---
+++ server.js
@@ -0,0 +1,8 @@
+﻿const express = require('express');
+const apiRoutes = require('./src/routes/api.v1');
+const app = express();
+
+app.use(express.json());
+app.use('/api/v1', apiRoutes); // All routes start with /api/v1/
+
+app.listen(3000, () => console.log("PaaS Backend Online"));
