e02ebc4eb1cd71b1db9bec9b5267031a62ed61e1
alex <alexander.vrabel@proton.me>
2026-04-04 20:09:58 +0200

    initial commit

---
+++ src/config/k8s.js
@@ -0,0 +1,10 @@
+﻿const k8s = require('@kubernetes/client-node');
+
+const kc = new k8s.KubeConfig();
+kc.loadFromDefault();
+
+const coreApi = kc.makeApiClient(k8s.CoreV1Api);   // Namespaces, Pods
+const appsApi = kc.makeApiClient(k8s.AppsV1Api);   // Deployments
+const networkApi = kc.makeApiClient(k8s.NetworkingV1Api); // Ingress (Traefik)
+
+module.exports = { coreApi, appsApi, networkApi };
